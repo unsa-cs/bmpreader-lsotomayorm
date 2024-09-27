3f15d9ca2231824e5b56808f692b40c2d13be108
Luis Sotomayor Moya <luis.sotomayor.moya@gmail.com>
2024-09-26 19:09:36 -0500

    Se agrego el codigo para que el programa solicite 1 argumento, el nombre de la imagen. Queda pendiente agregar mas restricciones

--- main.c
+++ main.c
@@ -12,19 +12,31 @@ void display() {
   glFlush();
 }
 
-int main() {
-  char filename[256];
+int main(int argc, char *argv[]) {
+  //char filename[256];
+
+  if (argc > 0 && argc < 2) {
+    printf("Uso: %s <archivo.bmp>\n", argv[0]);
+    return 1;
+  } else {
+    printf("Cantidad de argumentos incorrecta! Solo debe haber 1 argumento.\n");
+    return 0;
+  }
+
+  char *filename = argv[1];
 
   // Pedir al usuario el nombre del archivo BMP
+  /* Se comenta como requisito del ejercicio 1
   printf("Ingrese el nombre del archivo BMP (con extensión): ");
   scanf("%255s", filename);
+  */
 
   image = readBMP(filename);
   if (!image) return 1;
 
   // Inicializar GLUT
-  int argc = 1; // Necesario para evitar problemas con glutInit
-  char *argv[1] = { "" }; // Argumento vacío para GLUT
+  //int argc = 1; // Necesario para evitar problemas con glutInit  	SABOTAJEEE
+  //char *argv[1] = { "" }; // Argumento vacío para GLUT		SABOTAJE!! >:V
   glutInit(&argc, argv);
 
   // Establecer el modo de visualización
